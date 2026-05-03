f58f44ffb3ddc6abb9d7303baabcab90d23dd3b8
joaquinchecura <joaquinchecura@gmail.com>
2026-05-03 16:55:58 -0300

    Integrate Vercel Analytics and SpeedInsights
    Added Analytics and SpeedInsights components for tracking.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
 )
